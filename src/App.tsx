e105acc318b6e6e0ee48bb15fdb8c84c5fb556d2
Thigo Dev <thiagomvk08@gmail.com>
2025-01-12 12:58:38 -0300

    feat: layout done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { WiCloudy, WiDaySunny } from "react-icons/wi";
+import { WiBarometer, WiCloudy, WiDaySunny, WiHumidity } from "react-icons/wi";
 import "./App.css";
 import { FaSearch } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
+import { FiWind } from "react-icons/fi";
+import { MdVisibility } from "react-icons/md";
 
 function App() {
   return (
@@ -15,7 +17,7 @@ function App() {
           <input
             type="text"
             placeholder="Search for place"
-            className="bg-opacity-50 p-2 border-none outline-none rounded-lg w-full mr-2 placeholder:"
+            className="bg-opacity-50 p-2 border-none outline-none rounded-lg w-full mr-2"
           />
           <button className="flex items-center gap-2 px-4 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300">
             <FaSearch />
@@ -41,6 +43,48 @@ function App() {
 
       {/* right */}
       <div className="w-2/3 bg-red-100 p-4 flex flex-col">
+        {/* highlights */}
+        <div className="bg-red-600 h-1/4 p-4 text-center flex flex-col justify-center w-full">
+          <div>
+            <h3>Today's Highlights</h3>
+          </div>
+          <div className="bg-pink-700 w-full flex flex-row justify-center gap-2 mt-2.5">
+            <div className="border flex flex-col p-1 items-center w-1/5">
+              <p>wind status</p>
+              <p className="flex items-center gap-2">
+                <FiWind />
+                5km/h
+              </p>
+            </div>
+            <div className="border flex flex-col p-1 items-center w-1/5">
+              <p>humidity</p>
+              <p className="flex items-center gap-2">
+                <WiHumidity />
+                82%
+              </p>
+              {/* <div className="flex text-sm w-full justify-between text-[9px] font-bold">
+                <span>0</span>
+                <span>50</span>
+                <span>100</span>
+              </div>
+              <input type="range" className="w-full" /> */}
+            </div>
+            <div className="border flex flex-col p-1 items-center w-1/5">
+              <p>visibility</p>
+              <p className="flex items-center gap-2">
+                <MdVisibility />
+                6,0miles
+              </p>
+            </div>
+            <div className="border flex flex-col p-1 items-center w-1/5">
+              <p>air pressure</p>
+              <p className="flex items-center gap-2">
+                <WiBarometer />
+                1021mb
+              </p>
+            </div>
+          </div>
+        </div>
         {/* forecast */}
         <div className="bg-blue-950 h-1/3 p-4 flex flex-col justify-center items-center text-white">
           <div>
@@ -88,10 +132,6 @@ function App() {
               </div>
             </div>
           </div>
-        </div>
-        {/* highlights */}
-        <div className="bg-red-600 h-1/4 p-4">
-          <p>Highlights</p>
         </div>
         {/* graph */}
         <div className="bg-green-600 h-1/2 p-4">
