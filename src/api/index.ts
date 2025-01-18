a8b34801d621f6838deb5c73c656d4697bd7b851
Thigo Dev <thiagomvk08@gmail.com>
2025-01-18 17:57:01 -0300

    feat: functions to get weather

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,24 +1,35 @@
-/*
-  URLS: WEATHER `${BASE_URL}/weather?q=${cityName}&appid=${API_KEY}&units=metric` 
-        FORECAST `${BASE_URL}/forecast?q=${cityName}&appid=${API_KEY}&units=metric`
-*/
-
 import axios from "axios";
 
-const API_URL = process.env.API_URL;
-const API_KEY = process.env.API_KEY;
+const API_URL = import.meta.env.VITE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
 /**
  * Fetch current weather data by city name.
  * @param cityName - The name of the city
  * @returns Weather data as a JSON object
  */
-export const getWeather = async (cityName: string): Promise<void> => {
+export const getWeather = async (cityName: string) => {
   try {
     const response = axios.get(
       `${API_URL}/weather?q=${cityName}&appid=${API_KEY}&units=metric`
     );
-    console.log("response --->", response);
+    const data = (await response).data;
+    return data;
   } catch (error) {
     console.error("Error fetching weather data:", error);
   }
+};
+
+export const getWeatherByCoordinates = async (
+  latitude: number,
+  longitude: number
+) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+  );
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return await response.json();
 };
