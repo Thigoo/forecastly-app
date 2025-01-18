a9d3b4192d0429d554fcc651354811d8db88a176
Thigo Dev <thiagomvk08@gmail.com>
2025-01-18 18:00:44 -0300

    feat: display today weather

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,19 +1,42 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
-export const Search = () => {
+export const Search = ({ onSearch }: { onSearch(input: string): void }) => {
+  const [input, setInput] = useState("");
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (input.trim()) {
+      onSearch(input);
+      setInput("");
+    }
+  };
+
   return (
     <>
-      <div className="bg-purple-400 flex  justify-between w-full p-2 m-3">
+      <form
+        className="bg-purple-400 flex  justify-between w-full p-2 m-3"
+        onSubmit={handleSubmit}
+      >
         <input
           type="text"
           placeholder="Search for place"
           className="bg-opacity-50 p-2 border-none outline-none rounded-lg w-full mr-2"
+          value={input}
+          onChange={handleInput}
         />
-        <button className="flex items-center gap-2 px-4 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300">
+        <button
+          className="flex items-center gap-2 px-4 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300"
+          type="submit"
+        >
           <FaSearch />
           Search
         </button>
-      </div>
+      </form>
     </>
   );
 };
--- src/components/TodaysWeather/index.tsx
+++ src/components/TodaysWeather/index.tsx
@@ -1,15 +1,96 @@
 import { Header } from "../Header";
 import { Search } from "../Search";
 import { TodaysWeatherInfo } from "../TodaysWeatherInfo";
+import { getWeather, getWeatherByCoordinates } from "../../api";
+import { useEffect, useState } from "react";
+import { formatTemp } from "../../utils";
+
+interface IWeatherDataProps {
+  weather: {
+    main: string;
+  }[];
+  main: {
+    temp: number;
+  };
+  name: string;
+}
 
 export const TodaysWeather = () => {
+  const [weatherData, setWeatherData] = useState<IWeatherDataProps | null>(
+    null
+  );
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSearch = async (input: string) => {
+    try {
+      setLoading(true);
+      const data = await getWeather(input);
+      console.log("data ->", data);
+      setWeatherData(data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      setError("Could not fetch weather data. Please try again");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const fetchWeatherForCurrentLocation = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        if (!navigator.geolocation) {
+          throw new Error("Geolocation is not supported by this browser.");
+        }
+
+        navigator.geolocation.getCurrentPosition(
+          async (position) => {
+            const { latitude, longitude } = position.coords;
+            const response = await getWeatherByCoordinates(latitude, longitude);
+            setWeatherData(response);
+          },
+          () => {
+            handleSearch("London");
+          }
+        );
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+        setError("Could not fetch weather data. Defaulting to London");
+        handleSearch("London");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchWeatherForCurrentLocation();
+  }, []);
+
+  const formattedTemp = weatherData
+    ? formatTemp(weatherData?.main.temp as number)
+    : 0;
   return (
     <>
       {/* left */}
       <div className="w-1/3 bg-blue-100 p-4 flex flex-col text-center items-center">
         <Header />
-        <Search />
-        <TodaysWeatherInfo />
+        <Search onSearch={handleSearch} />
+        {loading && (
+          <div className="flex flex-col items-center justify-center h-64">
+            <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+            <p className="text-blue-500">Loading weather data...</p>
+          </div>
+        )}
+        {error && (
+          <div className="bg-red-500 text-white p-4 rounded-md mt-4">
+            <p>{error}</p>
+          </div>
+        )}
+        <TodaysWeatherInfo
+          temp={formattedTemp}
+          main={(weatherData?.weather[0].main as string) || "N/A"}
+          location={(weatherData?.name as string) || "Unknown"}
+        />
       </div>
     </>
   );
